23750654d373205b6ec6a52b79aba6aa5193b480
Trupti_SDE <truptinawadkar5@gmail.com>
2024-11-02 22:18:59 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Lab7_EFCore_AutoMapper.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
